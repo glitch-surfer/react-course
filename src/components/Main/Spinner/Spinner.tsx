dd995d010dfc9fac3eef5314195cfad9785faff4
vladislav_garaev <vladislav_garaev@epam.com>
2025-02-02 20:12:46 +0100

    feat: implement Spinner.tsx

---
+++ src/components/Main/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import './Spinner.css';
+
+class Spinner extends Component {
+  render() {
+    return <div className="spinner"></div>;
+  }
+}
+
+export default Spinner;
