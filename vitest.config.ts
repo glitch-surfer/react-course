86897f7a98e2cf8c503a4dba7e23ebede76f6253
vladislav_garaev <vladislav_garaev@epam.com>
2025-02-06 14:32:57 +0100

    chore: add test: coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'istanbul',
+      include: ['src/**/*.tsx'], // Include all .tsx files in the `src` directory (recursively)
+      exclude: [
+        '**/node_modules/**', // Exclude node_modules
+        'src/**/*.test.tsx', // Exclude test files
+        'src/__mocks__/**', // Optionally exclude mock files if using mocks
+      ],
+    },
     browser: {
       enabled: true,
       provider: 'playwright',
